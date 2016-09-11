7b33d4eb8853d9cfac5f2a06e8331f7109d3b8bb
Mihail Zakharov <mihail.zaharov84@mail.ru>
2016-09-11 14:55:21 +0500

    Set bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #match '/index', to: 'users#index', via: 'get'
+  get 'users/index'
 end
